7f5899b66a60bd8883704f201c02a8c855aa2d3f
V1K1NGbg <victor@ilchev.com>
2020-12-10 21:00:01 +0200

    idk
    https://codepen.io/Lewitje/pen/BNNJjo

--- script.js
+++ script.js
@@ -1,9 +1,120 @@
+function sin() {
+    var x = document.getElementById("number").value;
+
+    document.getElementById("number").innerHTML = x;
+}
+function cos() {
+    var x = document.getElementById("number").value;
+    
+    document.getElementById("number").innerHTML = x;
+}
+function tan() {
+    var x = document.getElementById("number").value;
+    
+    document.getElementById("number").innerHTML = x;
+}
+function devide() {
+    var x = document.getElementById("number").value;
+    
+    document.getElementById("number").innerHTML = x;
+}
+function sinneg() {
+    var x = document.getElementById("number").value;
+    
+    document.getElementById("number").innerHTML = x;
+}
+function cosneg() {
+    var x = document.getElementById("number").value;
+    
+    document.getElementById("number").innerHTML = x;
+}
+function tanneg() {
+    var x = document.getElementById("number").value;
+    
+    document.getElementById("number").innerHTML = x;
+}
+function multiply() {
+    var x = document.getElementById("number").value;
+    
+    document.getElementById("number").innerHTML = x;
+}
+function seven() {
+    var x = document.getElementById("number").value;
+    
+    document.getElementById("number").innerHTML = x;
+}
+function eight() {
+    var x = document.getElementById("number").value;
+    
+    document.getElementById("number").innerHTML = x;
+}
+function nine() {
+    var x = document.getElementById("number").value;
+    
+    document.getElementById("number").innerHTML = x;
+}
+function minus() {
+    var x = document.getElementById("number").value;
+    
+    document.getElementById("number").innerHTML = x;
+}
+function four() {
+    var x = document.getElementById("number").value;
+    
+    document.getElementById("number").innerHTML = x;
+}
+function five() {
+    var x = document.getElementById("number").value;
+    
+    document.getElementById("number").innerHTML = x;
+}
+function six() {
+    var x = document.getElementById("number").value;
+    
+    document.getElementById("number").innerHTML = x;
+}
+function plus() {
+    var x = document.getElementById("number").value;
+    
+    document.getElementById("number").innerHTML = x;
+}
+function one() {
+    var x = document.getElementById("number").value;
+    
+    document.getElementById("number").innerHTML = x;
+}
+function two() {
+    var x = document.getElementById("number").value;
+    
+    document.getElementById("number").innerHTML = x;
+}
+function three() {
+    var x = document.getElementById("number").value;
+    
+    document.getElementById("number").innerHTML = x;
+}
 function equal() {
     var x = document.getElementById("number").value;
     
-
-
-
-
-    document.getElementById("test").innerHTML = x;
+    document.getElementById("number").innerHTML = x;
+}
+function zero() {
+    var x = document.getElementById("number").value;
+    
+    document.getElementById("number").innerHTML = x;
+}
+function dot() {
+    var x = document.getElementById("number").value;
+    
+    document.getElementById("number").innerHTML = x;
+}
+function reverse_sign() {
+    var x = document.getElementById("number").value;
+    
+    document.getElementById("number").innerHTML = x;
+}
+function percent() {
+    var x = document.getElementById("number").value;
+    
+    document.getElementById("number").innerHTML = x+10;
 }
